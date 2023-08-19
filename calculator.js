83bf3c68b0e1c5a9a990dbe4deab51e74d2dc5da
IMOIMI <142657321+IMOIMI@users.noreply.github.com>
2023-08-19 11:53:38 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,21 @@
+ (function() {
+    var screen = document.querySelector('.screen');
+    var buttons = document.querySelectorAll('.btn');
+    var equal = document.querySelector('.btn-equal');
+    var clear = document.querySelector('.btn-clear');
+buttons.forEach(function(button){
+   button.addEventListener('click', function(e){
+   var value =e.target.dataset.numbers;
+   screen.value += value;
+   })
+});
+equal.addEventListener('click', function(e){
+    if (screen.value === '') {
+        screen.value = "";
+    }else {
+        var answer= eval(screen.value);
+        screen.value = answer
+    }
+})
+
+ })();
